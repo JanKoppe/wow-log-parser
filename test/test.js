a20ea0ab8db860b691763a0e3cd5372f07160145
Jan Koppe <post@jankoppe.de>
2017-02-10 23:13:58 +0100

    basic structure

---
+++ test/test.js
@@ -0,0 +1,11 @@
+const assert = require('assert')
+const parse = require('../lib/parse')
+
+// This is just template-foo. replace with real tests.
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4))
+    });
+  });
+});
